1f7ba1e66cb32ab35c071c0f6e97d56141fd7b61
skush-1 <kushsumit27@gmail.com>
2024-07-31 11:33:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Student-Teacher Booking Appointment System
+
+This is a MERN stack project designed to facilitate the booking of appointments between students and teachers. The system includes functionalities for admins to manage teachers, for teachers to manage their appointments, and for students to book appointments with teachers.
---
+++ backend/routes/teacherRoutes.js
@@ -0,0 +1,20 @@
+const { setRole, allow } = require("../controllers/adminController")
+const { login, updatePassword, verifyToken } = require("../controllers/authController")
+const express = require('express')
+const { createAppointment, deleteAppointment, getAllAppointments, getAllStudents, approveAppointment, dissapproveAppointment, getAllPendingStudents, register } = require("../controllers/teacherController")
+const router = express.Router()
+
+router.route('/').get(getAllStudents);
+router.route('/register').post(register);
+
+router.post('/login', login)
+router.patch('/updatePassword', verifyToken, updatePassword)
+
+router.route('/schedule').get(verifyToken, allow('admin', 'teacher'), getAllAppointments).post(verifyToken, allow('admin', 'teacher'), createAppointment)
+
+router.route('/reschedule/:id').delete(verifyToken,allow('teacher'),deleteAppointment);
+
+router.route('/changeApprovalStatus/:id/:studentId').delete(verifyToken, allow('admin', 'teacher'), dissapproveAppointment).patch(verifyToken,allow('admin','teacher'),approveAppointment)
+
+router.route('/getAllPendingStudents').get(verifyToken,allow('teacher'),getAllPendingStudents);
+module.exports = router
